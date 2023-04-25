2a073518d43e6e1ba8fd5a3cb49b2a04bc507fd3
Robert Santos <robertdosantos09@gmail.com>
2023-04-25 16:38:51 -0300

    feat: Adicionando codigos a função study_schedule. Para a resolução do requisito 1.

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
-def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def study_schedule(permanence_period: list[tuple], target_time: int):
+    try:
+        result_count = 0
+        for period in permanence_period:
+            if (period[0] <= target_time <= period[1]):
+                result_count += 1
+
+        return result_count
+    except TypeError:
+        return None
