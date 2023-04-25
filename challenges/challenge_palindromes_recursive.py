b359eb42154a948663e831122d100c42ff4ec5c8
Robert Santos <robertdosantos09@gmail.com>
2023-04-25 17:52:52 -0300

    feat: Adicionando codigos a função is_palindrome_recursive. Para a resolução do requisito 2.

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,11 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    try:
+        if (high_index < low_index and word != ''):
+            return True
+        if (word[low_index] == word[high_index]):
+            return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+        else:
+            return False
+
+    except IndexError:
+        return False
