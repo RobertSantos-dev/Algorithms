b0db9fb11b0484aeb2818927adaaa2caded236a5
Robert Santos <robertdosantos09@gmail.com>
2023-04-26 19:01:48 -0300

    feat: Adicionando codigos a função find_duplicate. Para a resolução do requisito 5.

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,22 @@
+def max_num(nums):
+    max_value = (0, 0)
+    for i in nums:
+        if (i[1] > max_value[1]):
+            max_value = i
+    if (int(max_value[1]) == 1):
+        return False
+    return int(max_value[0])
+
+
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if (not nums or len(nums) == 1):
+        return False
+    order_nums = {}
+    for i in nums:
+        if (type(i) == str or i < 0):
+            return False
+        elif (str(i) in order_nums):
+            order_nums[f'{i}'] += 1
+        else:
+            order_nums[f'{i}'] = 1
+    return max_num(list(order_nums.items()))
